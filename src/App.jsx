4724657815b8ae109ab28eab99dbf3140d36c222
Diego-Fdez <dieghoxcr7@gmail.com>
2023-02-24 16:53:55 -0600

    add adminHome page

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css';
 import { NavbarScreen, CategoriesScreen } from './components';
+import { AdminHome } from './pages';
 
 function App() {
   return (
     <div className='container'>
       <NavbarScreen />
       <CategoriesScreen />
+      <AdminHome />
     </div>
   );
 }
