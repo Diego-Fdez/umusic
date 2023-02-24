4d98400fc40b1ab0539c589ca9d807179b9e8bc8
Diego-Fdez <dieghoxcr7@gmail.com>
2023-02-24 15:17:43 -0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import './App.css';
+import { NavbarScreen } from './components';
+
+function App() {
+  return (
+    <div className='container'>
+      <NavbarScreen />
+    </div>
+  );
+}
+
+export default App;
