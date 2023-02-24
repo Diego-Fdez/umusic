f9cd18945d0bb2a62e2bed3e48af25ae2650908f
Diego-Fdez <dieghoxcr7@gmail.com>
2023-02-24 16:40:11 -0600

    create categories component

---
+++ src/components/CategoriesScreen/CategoriesScreen.jsx
@@ -0,0 +1,17 @@
+import './styles/categoriesStyles.css';
+
+const CategoriesScreen = () => {
+  return (
+    <div className='categories-container'>
+      <button className='category-button'>All</button>
+      <button className='category-button'>Electronics</button>
+      <button className='category-button'>Fashion</button>
+      <button className='category-button'>Music</button>
+      <button className='category-button'>Reggae</button>
+      <button className='category-button'>Sports</button>
+      <button className='category-button'>Travel</button>
+    </div>
+  );
+};
+
+export default CategoriesScreen;
