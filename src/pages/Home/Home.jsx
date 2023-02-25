e2d5db3ad082e99255cb43a9812f888ef40edba2
Diego-Fdez <dieghoxcr7@gmail.com>
2023-02-24 21:31:00 -0600

    add categories screen

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,4 +1,5 @@
 import './styles/homeStyles.css';
+import { CategoriesScreen } from '../../components';
 import { VideoCard } from './components';
 import useVideoStore from '../../store/videoStore';
 
@@ -6,11 +7,14 @@ const Home = () => {
   const videos = useVideoStore((state) => state.videos);
   console.log(videos);
   return (
-    <main className='home-container'>
-      {videos.map((video) => (
-        <VideoCard key={video.id.videoId} video={video} />
-      ))}
-    </main>
+    <>
+      <CategoriesScreen />
+      <main className='home-container'>
+        {videos.map((video) => (
+          <VideoCard key={video.id.videoId} video={video} />
+        ))}
+      </main>
+    </>
   );
 };
 
