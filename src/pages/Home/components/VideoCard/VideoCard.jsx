7225130d382fa14882c21ea5b3f4f1182dd3371d
Diego-Fdez <dieghoxcr7@gmail.com>
2023-02-24 20:57:18 -0600

    add image and link

--- src/pages/Home/components/VideoCard/VideoCard.jsx
+++ src/pages/Home/components/VideoCard/VideoCard.jsx
@@ -1,9 +1,29 @@
+import { Link } from 'react-router-dom';
 import './styles/videoCardStyles.css';
 
-const VideoCard = ({ video }) => {
+const VideoCard = ({
+  video: {
+    id: { videoId },
+    snippet,
+  },
+}) => {
   return (
     <div className='video-card-container'>
-      <h1>Video</h1>
+      <Link to={`/video/${videoId}`}>
+        <img
+          src={snippet?.thumbnails?.high?.url}
+          alt={snippet?.title}
+          className='video-card-image'
+        />
+      </Link>
+      <div className='video-card-title'>
+        <Link to={`/video/${videoId}`}>
+          <h4 className='title'>{snippet?.title.slice(0, 60)}</h4>
+        </Link>
+        <Link to={`/channel/${snippet?.channelId}`}>
+          <h6 className='subtitle'>{snippet?.channelTitle}</h6>
+        </Link>
+      </div>
     </div>
   );
 };
