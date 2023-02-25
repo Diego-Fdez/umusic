55ea1b490c4cc020d4cbe955585f05d63e91a0b6
Diego-Fdez <dieghoxcr7@gmail.com>
2023-02-24 19:02:17 -0600

    create video card component

---
+++ src/pages/Home/components/VideoCard/VideoCard.jsx
@@ -0,0 +1,11 @@
+import './styles/videoCardStyles.css';
+
+const VideoCard = ({ video }) => {
+  return (
+    <div className='video-card-container'>
+      <h1>Video</h1>
+    </div>
+  );
+};
+
+export default VideoCard;
