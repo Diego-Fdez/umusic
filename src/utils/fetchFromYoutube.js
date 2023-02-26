b6b38fb45b2f47d2684a9b6f284b0f3987811ac3
Diego-Fdez <dieghoxcr7@gmail.com>
2023-02-26 12:47:48 -0600

    fix fetch function

--- src/utils/fetchFromYoutube.js
+++ src/utils/fetchFromYoutube.js
@@ -6,11 +6,11 @@ export const BASE_URL = `https://www.googleapis.com/youtube/v3`;
  * This function takes a url as an argument, and returns the data from the API.
  * @returns The data object from the axios call.
  */
-export const fetchFromYoutube = async (url, keyword) => {
-  const { data } = await axios(`${BASE_URL}/${url}`, {
+export const fetchFromYoutube = async (keyword) => {
+  const { data } = await axios(`${BASE_URL}/search`, {
     params: {
       part: 'snippet',
-      maxResults: 20,
+      maxResults: 24,
       key: import.meta.env.VITE_YOUTUBE_API_KEY,
       q: keyword,
     },
