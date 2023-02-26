3759378c5c1630a5aee3216e6652f447181f4fc7
Diego-Fdez <dieghoxcr7@gmail.com>
2023-02-25 20:35:45 -0600

    change dynamic url

--- src/pages/Home/components/VideoCard/VideoCard.jsx
+++ src/pages/Home/components/VideoCard/VideoCard.jsx
@@ -1,24 +1,19 @@
 import { Link } from 'react-router-dom';
 import './styles/videoCardStyles.css';
 
-const VideoCard = ({
-  video: {
-    id: { videoId },
-    snippet,
-  },
-}) => {
+const VideoCard = ({ video: { id, snippet } }) => {
   return (
     <div className='video-card-container'>
-      <Link to={`/video/${videoId}`}>
+      <Link to={`/playlist/${id}`}>
         <img
           src={snippet?.thumbnails?.high?.url}
-          alt={snippet?.title}
+          alt={snippet?.localized?.title}
           className='video-card-image'
         />
       </Link>
       <div className='video-card-title'>
-        <Link to={`/video/${videoId}`}>
-          <h4 className='title'>{snippet?.title.slice(0, 60)}</h4>
+        <Link to={`/playlist/${id}`}>
+          <h4 className='title'>{snippet?.localized?.title.slice(0, 60)}</h4>
         </Link>
         <Link to={`/channel/${snippet?.channelId}`}>
           <h6 className='subtitle'>{snippet?.channelTitle}</h6>
