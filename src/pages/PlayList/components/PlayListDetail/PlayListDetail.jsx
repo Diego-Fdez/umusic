aeaa54c411564478cfc50faed2f7e00319ee84d8
Diego-Fdez <dieghoxcr7@gmail.com>
2023-02-26 12:47:18 -0600

    create playlist detail

---
+++ src/pages/PlayList/components/PlayListDetail/PlayListDetail.jsx
@@ -0,0 +1,20 @@
+import './styles/playListDetail.css';
+
+const PlayListDetail = () => {
+  return (
+    <div className='playlistDetail-container'>
+      <h2>Your Playlist</h2>
+      <div>
+        <div>
+          <img src='' alt='' />
+        </div>
+        <div>
+          <h4>Song</h4>
+          <h6>Channel</h6>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PlayListDetail;
