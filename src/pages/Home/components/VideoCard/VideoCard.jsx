5f3d9c744b3e932962ff3d0a91f3bf9def8429b6
Diego-Fdez <dieghoxcr7@gmail.com>
2023-02-26 12:46:06 -0600

    fix props

--- src/pages/Home/components/VideoCard/VideoCard.jsx
+++ src/pages/Home/components/VideoCard/VideoCard.jsx
@@ -2,24 +2,37 @@ import { Link } from 'react-router-dom';
 import './styles/videoCardStyles.css';
 
 const VideoCard = ({ video: { id, snippet } }) => {
+  console.log(id);
   return (
-    <div className='video-card-container'>
-      <Link to={`/playlist/${id}`}>
-        <img
-          src={snippet?.thumbnails?.high?.url}
-          alt={snippet?.localized?.title}
-          className='video-card-image'
-        />
-      </Link>
-      <div className='video-card-title'>
-        <Link to={`/playlist/${id}`}>
-          <h4 className='title'>{snippet?.localized?.title.slice(0, 60)}</h4>
+    <>
+      <div className='video-card-container'>
+        <Link to={`/playlist/${id.videoId ? id.videoId : id}`}>
+          <img
+            src={
+              snippet?.thumbnails?.high?.url
+                ? snippet?.thumbnails?.high?.url
+                : snippet?.thumbnails?.default?.url
+            }
+            alt={
+              snippet?.localized ? snippet?.localized?.title : snippet?.title
+            }
+            className='video-card-image'
+          />
         </Link>
-        <Link to={`/channel/${snippet?.channelId}`}>
-          <h6 className='subtitle'>{snippet?.channelTitle}</h6>
-        </Link>
+        <div className='video-card-title'>
+          <Link to={`/playlist/${id.videoId ? id.videoId : id}`}>
+            <h4 className='title'>
+              {snippet?.localized
+                ? snippet?.localized?.title.slice(0, 60)
+                : snippet?.title.slice(0, 60)}
+            </h4>
+          </Link>
+          <Link to={`/channel/${snippet?.channelId}`}>
+            <h6 className='subtitle'>{snippet?.channelTitle}</h6>
+          </Link>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
