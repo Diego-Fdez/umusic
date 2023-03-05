2de51f1a2c2afae35be84545bc802caec9808342
Diego-Fdez <dieghoxcr7@gmail.com>
2023-03-04 19:57:42 -0600

    change endpoint

--- src/hooks/useFetch.jsx
+++ src/hooks/useFetch.jsx
@@ -2,21 +2,49 @@ import { useEffect, useState } from 'react';
 import useVideoStore from '../store/videoStore';
 
 export function useFetch(url) {
-  const baseURL = 'https://www.googleapis.com/youtube/v3';
+  const baseURL = 'https://youtube138.p.rapidapi.com';
   const addVideos = useVideoStore((state) => state.addVideos);
   const loading = useVideoStore((state) => state.setLoading);
+  const setError = useVideoStore((state) => state.setError);
   const [getData, setGetData] = useState([]);
+  const [controller, setController] = useState(null);
 
   useEffect(() => {
+    const options = {
+      method: 'GET',
+      headers: {
+        'X-RapidAPI-Key': import.meta.env.VITE_RAPID_API_KEY,
+        'X-RapidAPI-Host': import.meta.env.VITE_RAPID_API_HOST,
+      },
+    };
+
+    const abortController = new AbortController();
+    setController(abortController);
     loading(true);
-    fetch(`${baseURL}/${url}&key=${import.meta.env.VITE_YOUTUBE_API_KEY}`)
+    fetch(`${baseURL}/${url}`, options, {
+      signal: abortController.signal,
+    })
       .then((response) => response.json())
       .then((data) => {
-        setGetData(data?.items), addVideos(data?.items);
+        setGetData(data), addVideos(data?.contents), setError(data);
       })
-      .catch((error) => console.log(error))
+      .catch((error) => {
+        if (error.name === 'AbortError') {
+          console.log('Request was cancelled');
+        }
+        setError(error);
+      })
       .finally(() => loading(false));
+
+    return () => abortController.abort();
   }, [url]);
 
-  return { getData };
+  const handleCancelRequest = () => {
+    if (controller) {
+      controller.abort();
+      setError('Request was cancelled');
+    }
+  };
+
+  return { getData, handleCancelRequest };
 }
