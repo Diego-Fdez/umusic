c22977deb635fd790426c56c64294f41166ac449
Diego-Fdez <dieghoxcr7@gmail.com>
2023-03-04 19:58:30 -0600

    add mouse over to img

--- src/pages/Home/components/VideoCard/VideoCard.jsx
+++ src/pages/Home/components/VideoCard/VideoCard.jsx
@@ -1,34 +1,54 @@
 import { Link } from 'react-router-dom';
 import './styles/videoCardStyles.css';
+import { formattedTime } from '../../../../utils/formattedTime';
 
-const VideoCard = ({ video: { id, snippet } }) => {
+const VideoCard = ({ video }) => {
   return (
     <>
       <div className='video-card-container'>
-        <Link to={`/playlist/${id.videoId ? id.videoId : id}`}>
+        <Link to={`/watch/${video?.videoId}`}>
           <img
-            src={
-              snippet?.thumbnails?.high?.url
-                ? snippet?.thumbnails?.high?.url
-                : snippet?.thumbnails?.default?.url
-            }
-            alt={
-              snippet?.localized ? snippet?.localized?.title : snippet?.title
-            }
+            src={video?.thumbnails[0]?.url}
+            alt={video?.videoId}
             className='video-card-image'
+            onMouseOver={(e) =>
+              (e.target.src =
+                video?.movingThumbnails !== null
+                  ? video?.movingThumbnails[0]?.url
+                  : video?.thumbnails[0]?.url)
+            }
+            onMouseLeave={(e) => (e.target.src = video?.thumbnails[0]?.url)}
           />
         </Link>
+        <div className='duration-container'>
+          <p>{formattedTime(video?.lengthSeconds)}</p>
+        </div>
         <div className='video-card-title'>
-          <Link to={`/playlist/${id.videoId ? id.videoId : id}`}>
-            <h4 className='title'>
-              {snippet?.localized
-                ? snippet?.localized?.title.slice(0, 60)
-                : snippet?.title.slice(0, 60)}
-            </h4>
+          <Link to={`/watch/${video?.videoId}`}>
+            <h4 className='title'>{video?.title.slice(0, 60)}</h4>
           </Link>
-          <Link to={`/channel/${snippet?.channelId}`}>
-            <h6 className='subtitle'>{snippet?.channelTitle}</h6>
-          </Link>
+          <div className='author-container'>
+            <img
+              src={video?.author?.avatar[0]?.url}
+              alt={video?.author?.title}
+              className='author-image'
+            />
+            <Link to={`/channel/${video?.author?.channelId}`}>
+              <h6 className='subtitle'>{video?.author?.title}</h6>
+            </Link>
+          </div>
+          <div className='stats-container'>
+            <span>
+              {video?.stats?.views
+                ? `${Number(video?.stats?.views).toLocaleString(
+                    'es-US'
+                  )} views `
+                : '0 views'}
+            </span>
+            <span>
+              {video?.publishedTimeText ? ` - ${video?.publishedTimeText}` : ''}
+            </span>
+          </div>
         </div>
       </div>
     </>
