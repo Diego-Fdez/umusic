3f5828085369c02111baab4e40f8a07ed60f8efa
Diego-Fdez <dieghoxcr7@gmail.com>
2023-03-07 19:22:34 -0600

    add google auth provider

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import { lazy, Suspense } from 'react';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { GoogleOAuthProvider } from '@react-oauth/google';
 import './App.css';
 import userStore from './store/userStore';
 import { Loader } from './components';
@@ -14,19 +15,21 @@ function App() {
   const user = userStore((state) => state.user);
 
   return (
-    <div className='container'>
-      <Suspense fallback={<Loader />}>
-        <BrowserRouter>
-          <Routes>
-            <Route path='/' index element={<Home />} />
-            <Route path='login' element={<Login />} />
-            <Route path='watch/:id' element={<PlayerScreen />} />
-            <Route path='/:rest/*' element={<ErrorPage />} />
-            <Route path='room' element={<Room />} />
-          </Routes>
-        </BrowserRouter>
-      </Suspense>
-    </div>
+    <GoogleOAuthProvider clientId={import.meta.env.VITE_GOOGLE_CLIENT_ID}>
+      <div className='container'>
+        <Suspense fallback={<Loader />}>
+          <BrowserRouter>
+            <Routes>
+              <Route path='/' index element={<Home />} />
+              <Route path='login' element={<Login />} />
+              <Route path='watch/:id' element={<PlayerScreen />} />
+              <Route path='/:rest/*' element={<ErrorPage />} />
+              <Route path='room' element={<Room />} />
+            </Routes>
+          </BrowserRouter>
+        </Suspense>
+      </div>
+    </GoogleOAuthProvider>
   );
 }
 
