7ffac733d819e4568af7efe578e7dada831fd105
Diego-Fdez <dieghoxcr7@gmail.com>
2023-03-10 19:31:49 -0600

    fix export files

--- src/components/index.js
+++ src/components/index.js
@@ -1,4 +1,4 @@
-export * from './CategoriesScreen';
-export * from './HeadScreen';
-export * from './Loader';
-export * from './NavbarScreen';
+export { default as CategoriesScreen } from './CategoriesScreen/CategoriesScreen';
+export { default as HeadScreen } from './HeadScreen/HeadScreen';
+export { default as Loader } from './Loader/Loader';
+export { default as NavbarScreen } from './NavbarScreen/NavbarScreen';
