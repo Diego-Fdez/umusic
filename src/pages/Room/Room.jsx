ff74c20a93356837b62ca2ef59882564b4277ee4
Diego-Fdez <dieghoxcr7@gmail.com>
2023-03-10 19:37:40 -0600

    create room page

--- src/pages/Room/Room.jsx
+++ src/pages/Room/Room.jsx
@@ -1,7 +1,41 @@
-import React from 'react';
+import { useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import ReactPlayer from 'react-player';
+import './styles/roomStyles.css';
+import { NavbarScreen } from '../../components';
+import { VideoHeader, RoomVideoList } from './components';
+import UseFetchFromDB from '../../hooks/useFetchFromDB';
 
 const Room = () => {
-  return <div>Room</div>;
+  const { id } = useParams();
+  const { getVideoList } = UseFetchFromDB();
+
+  useEffect(() => {
+    getVideoList(id);
+  }, []);
+
+  return (
+    <>
+      <NavbarScreen />
+      <main className='room-container'>
+        <article className='player-video-container'>
+          <div className='player-wrapper'>
+            <ReactPlayer
+              url={`https://www.youtube.com/watch?v=`}
+              width='100%'
+              height='100%'
+              controls={true}
+              loop
+            />
+          </div>
+          <VideoHeader />
+        </article>
+        <aside>
+          <RoomVideoList />
+        </aside>
+      </main>
+    </>
+  );
 };
 
 export default Room;
