3eaffccfe6252adc43ca3fe1f59d38b3e02ac03b
Diego-Fdez <dieghoxcr7@gmail.com>
2023-03-12 11:34:34 -0600

    add data persistence

--- src/store/userStore.js
+++ src/store/userStore.js
@@ -1,12 +1,14 @@
 import { create } from 'zustand';
+import Cookies from 'js-cookie';
 
 const userStore = create((set) => ({
-  user: {},
+  user: Cookies.get('user') ? JSON.parse(Cookies.get('user')) : {},
   loadingUser: false,
   addUser: (data) => {
     set((state) => ({
       user: data,
     }));
+    Cookies.set('user', JSON.stringify(data), { expires: 300 });
   },
   setLoadingUser: (data) => {
     set((state) => ({
--- src/store/videoStore.js
+++ src/store/videoStore.js
@@ -1,16 +1,25 @@
 import { create } from 'zustand';
+import Cookies from 'js-cookie';
 
 const useVideoStore = create((set) => ({
-  videos: [],
+  videos: Cookies.get('videos') ? JSON.parse(Cookies.get('videos')) : [],
   selectedCategory: 1,
   loading: false,
   error: null,
   keyword: 'todos',
   videoList: [],
+  currentVideoIndex: Cookies.get('currentVideoIndex')
+    ? JSON.parse(Cookies.get('currentVideoIndex'))
+    : 0,
+  shouldAutoPlay: true,
+  currentVideoPlaying: Cookies.get('currentVideoPlaying')
+    ? JSON.parse(Cookies.get('currentVideoPlaying'))
+    : {},
   addVideos: (data) => {
     set((state) => ({
       videos: data,
     }));
+    Cookies.set('videos', JSON.stringify(data), { expires: 300 });
   },
   addSelectedCategory: (data) => {
     set((state) => ({
@@ -36,6 +45,23 @@ const useVideoStore = create((set) => ({
     set((state) => ({
       videoList: data,
     }));
+  },
+  setCurrentVideoIndex: (data) => {
+    set((state) => ({
+      currentVideoIndex: data,
+    }));
+    Cookies.set('currentVideoIndex', JSON.stringify(data), { expires: 300 });
+  },
+  setShouldAutoPlay: (data) => {
+    set((state) => ({
+      shouldAutoPlay: data,
+    }));
+  },
+  setCurrentVideoPlaying: (data) => {
+    set((state) => ({
+      currentVideoPlaying: data,
+    }));
+    Cookies.set('currentVideoPlaying', JSON.stringify(data), { expires: 300 });
   },
 }));
 
