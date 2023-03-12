cdc6b978421154ef7ee3907efb0737e16ce579a3
Diego-Fdez <dieghoxcr7@gmail.com>
2023-03-12 11:29:36 -0600

    add qr and temporary-auth pages

--- src/App.jsx
+++ src/App.jsx
@@ -10,6 +10,8 @@ const Login = lazy(() => import('./pages/Login/Login'));
 const PlayerScreen = lazy(() => import('./pages/PlayerScreen/PlayerScreen'));
 const ErrorPage = lazy(() => import('./pages/ErrorPage/ErrorPage'));
 const Room = lazy(() => import('./pages/Room/Room'));
+const QRScreen = lazy(() => import('./pages/QRScreen/QRScreen'));
+const TemporaryAuth = lazy(() => import('./pages/TemporaryAuth/TemporaryAuth'));
 
 function App() {
   const user = userStore((state) => state.user);
@@ -25,6 +27,8 @@ function App() {
               <Route path='watch/:id' element={<PlayerScreen />} />
               <Route path='/:rest/*' element={<ErrorPage />} />
               <Route path='room/:id' element={<Room />} />
+              <Route path='qr' element={<QRScreen />} />
+              <Route path='temporary-login?' element={<TemporaryAuth />} />
             </Routes>
           </BrowserRouter>
         </Suspense>
